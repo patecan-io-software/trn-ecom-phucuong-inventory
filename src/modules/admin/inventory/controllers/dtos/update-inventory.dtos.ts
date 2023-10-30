baf74af730fcdc31753eeab93c1ab6e5190ff0e2
Nguyen Duy Minh <minhnguyenduy99@gmail.com>
2023-10-30 23:59:46 +0700

    fix(admin/inventory): Add request body fallback for query DTO

--- src/modules/admin/inventory/controllers/dtos/update-inventory.dtos.ts
+++ src/modules/admin/inventory/controllers/dtos/update-inventory.dtos.ts
@@ -1,14 +1,23 @@
 import { SuccessResponseDTO, ValidateNonNegative } from '@libs'
 import { ApiProperty, PartialType } from '@nestjs/swagger'
-import { Type } from 'class-transformer'
+import { Transform, Type } from 'class-transformer'
 import { InventoryDTO } from './inventory.dtos'
-import { IsNotEmpty, IsNumber, Validate } from 'class-validator'
+import { IsNumber, Validate, ValidateIf, isEmpty } from 'class-validator'
+
+export class UpdateInventoryQueryDTO {
+	@ApiProperty()
+	inventory_stock: string
+}
 
 export class UpdateInventoryRequestDTO {
 	@ApiProperty()
-	@IsNumber()
-	@IsNotEmpty()
+	@IsNumber({
+		allowInfinity: false,
+		allowNaN: false,
+		maxDecimalPlaces: 0,
+	})
 	@Validate(ValidateNonNegative)
+	@ValidateIf((obj, val) => !isEmpty(val))
 	inventory_stock: number
 }
 
