adf4e78658c4d3ba7c3c01905c79d0e63722f73e
giatue112212 <giatue112212@gmail.com>
2024-04-10 23:12:04 +0700

    Update Auto Delete Ratin After 10 Minutes

--- src/modules/rating/services/rating-scheduler.service.ts
+++ src/modules/rating/services/rating-scheduler.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { Cron } from '@nestjs/schedule'
+import * as dotenv from 'dotenv'
 import { RatingRepository } from '../database/rating.repository'
 
 @Injectable()
 export class RatingScheduler {
 	private readonly logger = new Logger(RatingScheduler.name)
 
-	constructor(private readonly ratingRepository: RatingRepository) {}
+	constructor(private readonly ratingRepository: RatingRepository) {
+		dotenv.config()
+	}
 
-	@Cron('0 * * * *')
+	@Cron(process.env.CRON_SCHEDULE)
 	async deleteExpiredRejectedRatings() {
 		try {
-			const expiredTime = new Date(Date.now() - 10 * 60 * 1000)
+			const expiredTime = new Date(
+				Date.now() - parseInt(process.env.EXPIRED_TIME) * 1000,
+			)
 			const rejectedRatings =
 				await this.ratingRepository.getRejectedRatings(expiredTime)
 
